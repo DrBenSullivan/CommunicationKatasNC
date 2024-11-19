163c52f35bc55d0a01dcab0f954060d0898a4c1e
DrBenSullivan <ben.sullivan1992@gmail.com>
2024-11-19 16:04:02 +0000

    Add BubbleSort implementation.

---
+++ CommunicationKatas/BubbleSort.cs
@@ -0,0 +1,27 @@
+﻿namespace CommunicationKatas
+{
+	public class BubbleSort
+	{
+		public int[] Sort(int[] listToSort)
+		{
+			bool isListSorted = true;
+
+			for (int i = 0; i < listToSort.Length - 2; i++)
+			{
+				int currentValue = listToSort[i];
+				int nextValue = listToSort[i + 1];
+
+				if (currentValue > nextValue)
+				{
+					isListSorted = false;
+					listToSort[i] = nextValue;
+					listToSort[i + 1] = currentValue;
+				}
+			}
+
+			if (isListSorted) return listToSort;
+
+			return (Sort(listToSort));
+		}
+	}
+}
