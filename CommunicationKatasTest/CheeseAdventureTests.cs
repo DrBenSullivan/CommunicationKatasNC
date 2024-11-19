bffd040de213c537889b418568d03b860a495515
DrBenSullivan <ben.sullivan1992@gmail.com>
2024-11-19 17:35:01 +0000

    Refactor CheeseAdventure tests

---
+++ CommunicationKatasTest/CheeseAdventureTests.cs
@@ -0,0 +1,49 @@
+﻿using CommunicationKatas;
+using FluentAssertions;
+
+namespace CommunicationKatasTest
+{
+	internal class CheeseAdventureTests
+	{
+		CheeseAdventure cheeseAdventure = new();
+
+		public static TestCaseData[] cheesePartyDeterminerData =
+		[
+			new("---K-----M---C--", 6, 0, "No cheese"),
+			new("---K-----M---C--", 0, 6, "Cheese"),
+			new("---K-----M---C--", 1, 1, "Cheese"),
+			new("---K-----M---C--", 3, 1, "No cheese"),
+			new("---K---M---C--", 2, 1, "Cheese party!"),
+			new("---K-----M---C--", 6, 0, "No cheese"),
+			new("---K-----M---C--", 6, 0, "No cheese"),
+
+		];
+
+		[TestCaseSource(nameof(cheesePartyDeterminerData))]
+		public void CheesePartyDeterminerTest_ValidArguments_ReturnExpectedString(string grid, int catSpeed, int mouseSpeed, string expectedResult)
+		{
+			// Act
+			string actualResult = cheeseAdventure.CheesePartyDeterminer(grid, catSpeed, mouseSpeed);
+
+			// Assert
+			actualResult.Should().Be(expectedResult);
+		}
+
+
+		public static TestCaseData[] cheesePartyDeterminerExceptionData =
+		[
+			new("--M-----K--C", 2, 1),
+			new("---K-----M---C--", -4, -2)
+		];
+
+		[TestCaseSource(nameof(cheesePartyDeterminerExceptionData))]
+		public void CheesePartyDeterminerTest_InvalidGrid_ThrowsArgumentException(string grid, int catSpeed, int mouseSpeed)
+		{
+			// Arrange
+			Action test = () => cheeseAdventure.CheesePartyDeterminer(grid, catSpeed, mouseSpeed);
+
+			// Act & Assert
+			test.Should().Throw<ArgumentException>();
+		}
+	}
+}
---
+++ CommunicationKatasTest/CheeseAventureTestsV2.cs
@@ -0,0 +1,49 @@
+﻿using CommunicationKatas;
+using FluentAssertions;
+
+namespace CommunicationKatasTest
+{
+	internal class CheeseAventureTestsV2
+	{
+		CheeseAdventureV2 cheeseAdventureV2 = new();
+
+		public static TestCaseData[] cheesePartyDeterminerV2TestData =
+		[
+			new("---K-----M---C--", 6, 0, "No cheese"),
+			new("---K-----M---C--", 0, 6, "Cheese"),
+			new("---K-----M---C--", 1, 1, "Cheese"),
+			new("---K-----M---C--", 3, 1, "No cheese"),
+			new("---K---M---C--", 2, 1, "Cheese party!"),
+			new("---M-----K---C--", 6, 6, "Cheese"),
+			new("---M-----K---C--", -1, 1, "No cheese"),
+			new("---K----M--------C--", -1, 1, "Cheese"),
+			new("---K--C--M---", -1, 1, "Everyone goes hungry!")
+		];
+
+		[TestCaseSource(nameof(cheesePartyDeterminerV2TestData))]
+		public void CheesePartyDeterminerV2Test_ValidArguments_ReturnsExpectedResult(string grid, int catSpeed, int mouseSpeed, string expectedResult)
+		{
+			// Act
+			string actualResult = cheeseAdventureV2.CheesePartyDeterminerV2(grid, catSpeed, mouseSpeed);
+
+			// Assert
+			actualResult.Should().Be(expectedResult);
+		}
+
+		public static TestCaseData[] cheesePartyDeterminerV2ArgumentTestData =
+		[
+			new("--M--K---K--C"),
+			new("---K*-----M---C--")
+		];
+
+		[TestCaseSource(nameof(cheesePartyDeterminerV2ArgumentTestData))]
+		public void CheesePartyDeterminerV2Test_InvalidArguments_ReturnsArgumentException(string grid)
+		{
+			// Arrange
+			Action test = () => cheeseAdventureV2.CheesePartyDeterminerV2(grid, -2, 0);
+
+			// Assert & Act
+			test.Should().Throw<ArgumentException>();
+		}
+	}
+}
