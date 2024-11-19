dcbe47f9b204b7d175f8fbdb2d78b37b4863d159
digydigdogdead <carlyle.laurent@gmail.com>
2024-11-19 11:58:07 +0000

    Created a Cryptography class with a ROT13Encoder function, and tests for that function

---
+++ CommunicationKatas/Cryptography.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationKatas
+{
+    public class Cryptography
+    {
+        public string ROT13Encoder(string message)
+        {
+            string result = "";
+
+            foreach (char c in message)
+            {
+                int start = 0;
+                int end = 0;
+
+                if (Char.IsUpper(c))
+                {
+                    start = (int)'A';
+                    end = (int)'Z';
+                }
+
+                else if (Char.IsLower(c))
+                {
+                    start = (int)'a';
+                    end = (int)'z';
+                } else { result += c; continue; }
+
+                int current = (int)c;
+                current += 13;
+
+                if (current > end)
+                {
+                    current -= end;
+                    current += start - 1;
+                    result += (char)current;
+                }
+                else
+                {
+                    result += (char)current;
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CommunicationKatasTest/CryptographyTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunicationKatas;
+using FluentAssertions;
+
+namespace CommunicationKatasTest
+{
+    internal class CryptographyTests
+    {
+        Cryptography testEncoder = new Cryptography();
+        
+        [Test]
+        public void ROT13Test()
+        {
+            string result1 = testEncoder.ROT13Encoder("northcoders"); // should be "abegupbqref"
+            string result2 = testEncoder.ROT13Encoder("abegupbqref"); // should be "northcoders"
+            string result3 = testEncoder.ROT13Encoder("I'm sure glad nobody can read my secret ROT13 code."); // should return "V'z fher tynq abobql pna ernq zl frperg EBG13 pbqr."
+
+            result1.Should().Be("abegupbqref");
+            result2.Should().Be("northcoders");
+            result3.Should().Be("V'z fher tynq abobql pna ernq zl frperg EBG13 pbqr.");
+        }
+
+    }
+}
