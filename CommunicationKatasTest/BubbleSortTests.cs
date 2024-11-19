f7ca7f803de71196407ed920e83bb03e5935c433
DrBenSullivan <ben.sullivan1992@gmail.com>
2024-11-19 16:12:07 +0000

    Add bubble sort tests.

---
+++ CommunicationKatasTest/BubbleSortTests.cs
@@ -0,0 +1,23 @@
+﻿using CommunicationKatas;
+using FluentAssertions;
+
+namespace CommunicationKatasTest
+{
+	public class BubbleSortTests
+	{
+		private BubbleSort bubbleSort = new BubbleSort();
+
+		[Test]
+		public void BubbleSortTest()
+		{
+			var result1 = bubbleSort.Sort([1,2,3]);
+			var result2 = bubbleSort.Sort([3,2,1]);
+			var result3 = bubbleSort.Sort([10,9,8,7,6,5,4,3,2,1]);
+
+			result1.Should().BeEquivalentTo([1,2,3]);
+			result2.Should().BeEquivalentTo([1,2,3]);
+			result3.Should().BeEquivalentTo([1,2,3,4,5,6,7,8,9,10]);
+		}
+		
+	}
+}
