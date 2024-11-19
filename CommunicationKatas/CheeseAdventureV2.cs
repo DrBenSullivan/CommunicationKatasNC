c74a6d1f8f48c8116681108b9c86392f25d66e90
DrBenSullivan <ben.sullivan1992@gmail.com>
2024-11-19 15:03:07 +0000

    Add CheeseAdventureV2 unit tests and implementation

---
+++ CommunicationKatas/CheeseAdventureV2.cs
@@ -0,0 +1,76 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Text.RegularExpressions;
+
+namespace CommunicationKatas
+{
+	public class CheeseAdventureV2
+    {
+        public string CheesePartyDeterminerV2(string grid, int catSpeed, int mouseSpeed)
+        {
+            ValidateGrid(grid);
+
+            int catIndex = grid.IndexOf("K");
+            int mouseIndex = grid.IndexOf("M");
+            int cheeseIndex = grid.IndexOf("C");
+
+            bool isCatToLeftOfMouse = catIndex < mouseIndex;
+            bool couldCatCatchMouse = isCatToLeftOfMouse && catSpeed > 0 || !isCatToLeftOfMouse && catSpeed < 0;
+
+            bool isMouseToLeftOfCheese = mouseIndex < cheeseIndex;
+            bool couldMouseCatchCheese = isMouseToLeftOfCheese && mouseSpeed > 0 || isMouseToLeftOfCheese && mouseSpeed < 0;
+
+            if (!couldCatCatchMouse && !couldMouseCatchCheese) return "Everyone goes hungry!";
+
+            if (couldMouseCatchCheese && !couldCatCatchMouse) return "Cheese";
+
+            while (true)
+            {
+                catIndex += catSpeed;
+                mouseIndex += mouseSpeed;
+
+                if (catIndex >= cheeseIndex && mouseIndex >= cheeseIndex)
+                {
+                    return "Cheese party!";
+                }
+
+                else if (catIndex >= mouseIndex) { return "No cheese"; }
+
+                else if (mouseIndex >= cheeseIndex) { return "Cheese"; }
+            }
+        }
+
+        private void ValidateGrid(string grid)
+        {
+            char[] gridChars = grid.ToCharArray();
+
+            int catCount = 0;
+            int mouseCount = 0;
+            int cheeseCount = 0;
+            
+            foreach (char c in gridChars)
+            {
+                if (c == 'K')
+                {
+                    catCount++;
+                    continue;
+                }
+
+                if (c == 'M')
+                {
+                    mouseCount++;
+                    continue;
+                }
+
+                if (c == 'C')
+                {
+                    cheeseCount++;
+                    continue;
+                }
+
+                if (c != '-') throw new ArgumentException("The grid is invalid.");
+            }
+
+            if (catCount != 1 || mouseCount != 1 || cheeseCount != 1) throw new ArgumentException("The grid is invalid.");
+        }
+    }
+}
---
+++ CommunicationKatasTest/CheeseTestsV2.cs
@@ -0,0 +1,48 @@
+﻿using CommunicationKatas;
+using FluentAssertions;
+
+namespace CommunicationKatasTest
+{
+	internal class CheeseTestsV2
+    {
+        CheeseAdventureV2 testAdventure = new CheeseAdventureV2();
+        
+        [Test]
+        public void CheesePartyDeterminerV2Test()
+        {
+            string noCheeseResult = "No cheese";
+            string cheeseResult = "Cheese";
+            string cheesePartyResult = "Cheese party!";
+            string noCollisionsResult = "Everyone goes hungry!";
+
+            string result1 = testAdventure.CheesePartyDeterminerV2("---K-----M---C--", 6, 0); // should return "No cheese"
+            string result2 = testAdventure.CheesePartyDeterminerV2("---K-----M---C--", 0, 6); // should return "Cheese"
+            string result3 = testAdventure.CheesePartyDeterminerV2("---K-----M---C--", 1, 1); // should return "Cheese"
+            string result4 = testAdventure.CheesePartyDeterminerV2("---K-----M---C--", 3, 1); // should return "No cheese"
+            string result5 = testAdventure.CheesePartyDeterminerV2("---K---M---C--", 2, 1); // should return "Cheese party!"
+            string result6 = testAdventure.CheesePartyDeterminerV2("---M-----K---C--", 6, 6); // should return "Cheese"
+            string result7 = testAdventure.CheesePartyDeterminerV2("---M-----K---C--", -1, 1); // should return "No cheese"
+            string result8 = testAdventure.CheesePartyDeterminerV2("---K----M--------C--", -1, 1); // should return "Cheese"
+            string result9 = testAdventure.CheesePartyDeterminerV2("---K--C--M---", -1, 1); // should return "Everyone goes hungry!"
+
+            Action test6 = () => testAdventure.CheesePartyDeterminerV2("--M--K---K--C", 2, 1); // should string exception
+            Action test7 = () => testAdventure.CheesePartyDeterminerV2("---K*-----M---C--", -4, -2); // should throw speed exception
+
+
+            result1.Should().Be(noCheeseResult);
+            result2.Should().Be(cheeseResult);
+            result3.Should().Be(cheeseResult);
+            result4.Should().Be(noCheeseResult);
+            result5.Should().Be(cheesePartyResult);
+            result6.Should().Be(cheeseResult);
+            result7.Should().Be(noCheeseResult);
+            result8.Should().Be(cheeseResult);
+            result9.Should().Be(noCollisionsResult);
+
+            test6.Should().Throw<ArgumentException>();
+            test7.Should().Throw<ArgumentException>();
+
+
+        }
+    }
+}
